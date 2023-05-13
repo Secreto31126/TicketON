d0b687e0c319a07dd75b1be03aab6b6ca5260c86
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2023-05-13 16:20:15 -0300

    Code split

---
+++ src/lib/components/scanner.svelte
@@ -0,0 +1,109 @@
+<script lang="ts">
+	import QrScanner from 'qr-scanner';
+	import { spring } from 'svelte/motion';
+	import { fade, scale } from 'svelte/transition';
+	import { onMount, onDestroy, tick } from 'svelte';
+
+	export let success: boolean | null = false;
+	export let scan_active: boolean = false;
+	export let data: string = '';
+	export let duration = 1000;
+
+	let qrScanner: QrScanner;
+	let camera: HTMLVideoElement;
+	let camera_overlay: HTMLDivElement;
+	let camera_list: Awaited<ReturnType<typeof QrScanner.listCameras>> | null = null;
+	let selected_camera: string;
+
+	const scanner_animation =
+		success !== null ? new Promise<void>((r) => setTimeout(r, duration)) : Promise.reject();
+
+	let coords = spring(
+		{ x: 0, y: 0 },
+		{
+			stiffness: 0.1,
+			damping: 0.5
+		}
+	);
+
+	onMount(async () => {
+		if (!(await QrScanner.hasCamera())) {
+			alert('No se detectó ninguna cámara');
+			return;
+		}
+
+		qrScanner = new QrScanner(
+			camera,
+			(result) => {
+				data = result.data;
+				console.log(result.cornerPoints);
+				coords.set(result.cornerPoints[0]);
+			},
+			{
+				maxScansPerSecond: 1,
+				overlay: camera_overlay,
+				highlightCodeOutline: true,
+				returnDetailedScanResult: true
+			}
+		);
+
+		camera_list = await QrScanner.listCameras(true);
+
+		await tick();
+		scan_active = true;
+	});
+
+	onDestroy(() => {
+		qrScanner?.destroy();
+	});
+
+	$: if (scan_active) {
+		qrScanner?.start();
+	} else {
+		qrScanner?.stop();
+	}
+
+	$: if (selected_camera) {
+		qrScanner?.setCamera(selected_camera);
+	}
+</script>
+
+<!-- Camera Picker -->
+{#if camera_list && camera_list.length > 1}
+	<select name="camera" bind:value={selected_camera}>
+		{#each camera_list as { label, id }}
+			<option value={id}>{label}</option>
+		{/each}
+	</select>
+{/if}
+
+<!-- Camera -->
+<!-- svelte-ignore a11y-media-has-caption doesn't apply -->
+<video bind:this={camera} class:hide-video={!scan_active} class="h-[60vh]">
+	<div bind:this={camera_overlay}>
+		{#await scanner_animation}
+			<!-- Fade in -->
+			<div transition:fade>
+				<!-- Scale -->
+				<div in:scale>
+					{#if success}
+						<img src="/checkmark.svg" alt="Accepted" class="w-full h-full" />
+					{:else}
+						<img src="/cross.svg" alt="Rejected" class="w-full h-full" />
+					{/if}
+				</div>
+			</div>
+		{:then}
+			<!-- Fade out -->
+		{/await}
+	</div>
+</video>
+
+<style>
+	.hide-video {
+		/* Safari breaks if you use diplay: none; */
+		opacity: 0;
+		width: 0px;
+		height: 0px;
+	}
+</style>
--- src/routes/scan/+page.svelte
+++ src/routes/scan/+page.svelte
@@ -1,83 +1,26 @@
 <script lang="ts">
 	import type { PageData, ActionData } from './$types';
 
-	import QrScanner from 'qr-scanner';
 	import { enhance } from '$app/forms';
-	import { spring } from 'svelte/motion';
-	import { fade, scale } from 'svelte/transition';
-	import { onMount, onDestroy, tick } from 'svelte';
+	import Scanner from '$lib/components/scanner.svelte';
 
 	export let data: PageData;
 	export let form: ActionData;
-
-	const duration = 1000;
-	const scanner_animation = new Promise<void>((r) => (form ? setTimeout(r, duration) : r()));
-
-	let coords = spring(
-		{ x: 0, y: 0 },
-		{
-			stiffness: 0.1,
-			damping: 0.5
-		}
-	);
 
 	let form_html: HTMLFormElement;
 
 	let party = form?.party ?? '';
 	let ticket = '';
 
-	let qrScanner: QrScanner;
-	let camera: HTMLVideoElement;
-	let scan_active: boolean = false;
-	let camera_overlay: HTMLDivElement;
-	let camera_list: Awaited<ReturnType<typeof QrScanner.listCameras>> | null = null;
-	let selected_camera: string;
-	onMount(async () => {
-		if (!(await QrScanner.hasCamera())) {
-			alert('No se detectó ninguna cámara');
-			return;
-		}
-
-		qrScanner = new QrScanner(
-			camera,
-			(result) => {
-				ticket = result.data;
-				console.log(result.cornerPoints);
-				coords.set(result.cornerPoints[0]);
-			},
-			{
-				maxScansPerSecond: 1,
-				overlay: camera_overlay,
-				highlightCodeOutline: true,
-				returnDetailedScanResult: true
-			}
-		);
-
-		camera_list = await QrScanner.listCameras(true);
-
-		await tick();
-		scan_active = true;
-	});
-
-	onDestroy(() => {
-		qrScanner?.destroy();
-	});
-
-	$: if (scan_active) {
-		qrScanner?.start();
-	} else {
-		qrScanner?.stop();
-	}
-
-	$: if (selected_camera) {
-		qrScanner?.setCamera(selected_camera);
-	}
+	let scan_active: boolean;
 
 	let submit: HTMLButtonElement;
-	$: if (scan_active && ticket) {
+	$: if (scan_active && ticket && submit) {
 		console.log(party, ticket);
-		submit?.click();
+		submit.click();
 	}
+
+	$: success = form?.success ?? null;
 </script>
 
 <form class="flex flex-col items-center space-y-4" method="POST" use:enhance bind:this={form_html}>
@@ -89,36 +32,8 @@
 	</select>
 
 	{#if party}
-		<!-- Camera Picker -->
-		{#if camera_list}
-			<select name="camera" bind:value={selected_camera}>
-				{#each camera_list as { label, id }}
-					<option value={id}>{label}</option>
-				{/each}
-			</select>
-		{/if}
-
 		<!-- Camera -->
-		<!-- svelte-ignore a11y-media-has-caption doesn't apply -->
-		<video bind:this={camera} class:hide-video={!scan_active} class="h-[60vh]">
-			<div bind:this={camera_overlay}>
-				{#await scanner_animation}
-					<!-- Fade in -->
-					<div transition:fade>
-						<!-- Scale -->
-						<div in:scale>
-							{#if form?.success}
-								<img src="/checkmark.svg" alt="Accepted" class="w-full h-full" />
-							{:else if form}
-								<img src="/cross.svg" alt="Rejected" class="w-full h-full" />
-							{/if}
-						</div>
-					</div>
-				{:then}
-					<!-- Fade out -->
-				{/await}
-			</div>
-		</video>
+		<Scanner bind:data={ticket} bind:scan_active bind:success />
 
 		<!-- Toggle Camera -->
 		<button on:click|preventDefault={() => (scan_active = !scan_active)}>
@@ -152,12 +67,5 @@
 <style>
 	.hide {
 		display: none;
-	}
-
-	.hide-video {
-		/* Safari breaks if you use diplay: none; */
-		opacity: 0;
-		width: 0px;
-		height: 0px;
 	}
 </style>
