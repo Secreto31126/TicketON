b828b0e00f2e05804b2e36cbcfb446c6baeb54c6
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2023-05-14 05:46:44 -0300

    Added analytics
    'Cause why not?

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,8 @@
 <script>
 	import '../app.css';
+	import { inject } from '@vercel/analytics';
+
+	inject({ mode: import.meta.env.DEV ? 'development' : 'production' });
 </script>
 
 <div class="w-screen h-full overflow-x-hidden">
