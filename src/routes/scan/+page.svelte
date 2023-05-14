eaa01520d379dbd5bde569bcdd2f6951ee2fa3ea
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2023-05-14 03:56:00 -0300

    Another detail

--- src/routes/scan/+page.svelte
+++ src/routes/scan/+page.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
 	import type { PageData, ActionData } from './$types';
 
+	import { tick } from 'svelte';
 	import { enhance } from '$app/forms';
 	import Scanner from '$lib/components/scanner.svelte';
-	import { tick } from 'svelte';
 
 	export let data: PageData;
 	export let form: ActionData;
