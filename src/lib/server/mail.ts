1bc627768d511293b4460bebd09e58b1eccafebe
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2023-05-16 01:50:05 -0300

    Oh

--- src/lib/server/mail.ts
+++ src/lib/server/mail.ts
@@ -24,7 +24,7 @@ export default async function sendMail(
 		headers: { 'Content-Type': 'application/json' },
 		body: JSON.stringify({
 			API_TOKEN: GOOGLE_APP_SCRIPT_TOKEN,
-			to: 'tomyraiti@gmail.com',
+			to,
 			subject,
 			body,
 			images
